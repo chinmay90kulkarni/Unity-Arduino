5807014cef044799f8d0f7fd7c2ee72b2eb2a249
Chinmay Kulkarni <chinmay90kulkarni@gmail.com>
2017-08-26 22:58:09 +0530

    Sending data from Unity to Arduino using serial Communication (Unity Project)

---
+++ Assets/Script/EventHandling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class EventHandling : MonoBehaviour {
+	SerialPort sp = new SerialPort("COM3", 9600);
+
+	void Start()
+	{
+		sp.Open ();
+	}
+
+	public void hit()
+	{
+		sp.Write ("A");
+	}
+	public void miss()
+	{
+		sp.Write ("B");
+
+	}
+
+}
